964601db2b3cbe57ae9718e3f35940721815261f
Sean Worrell <spworrell@gmail.com>
2023-07-12 19:15:55 -0400

    Started Var/Const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
 // Assignment code here
 
+//Character Sets
+//var specialChr = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+const specialChr = "!@@$"
+const upperChr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const  lowerChr = "abcdefghijklmnopqrstuvwxyz"
+const numeric = "0123456789"
+
+
+
+// prompt user for password lenght and character types
+var pwdLength = prompt("Password length?")
+var usespecialChr = prompt("Use special characters?- Yes or No")
+var useupperChr = prompt("Use upper case letters?-- Yes or No")
+var uselowerChr = prompt("Use lower case letters?-- Yes or No")
+var useNumeric = prompt("Use numbers?-- Yes or No")
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +33,33 @@ function writePassword() {
 
 }
 
+
+// // quick way
+// students.forEach(student => console.log(`Welcome ${student}`))
+
+
+// lowerChr.forEach(letter); {
+
+//  console.log(letter);
+// }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
